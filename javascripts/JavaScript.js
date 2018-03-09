01273930454a0aa52890358c773d73314e1ecfd3
Bogdan5 <bogdan5x@gmail.com>
2018-03-08 22:38:30 -0500

    'Comment'

--- javascripts/JavaScript.js
+++ javascripts/JavaScript.js
@@ -30,6 +30,33 @@ $(document).ready(
       console.log('Fetch Error :-S', err);
     });
 
+  const lastPage=()=>Math.ceil(paintings.length/15);
+
+  $('.first-page').on('click',()=>{
+    fillContainer(1);
+    pageManager(1);
+  });
+  $('.previous').on('click',()=>{
+    fillContainer(--pageNo);
+    pageManager(--pageNo);
+  });
+  $('.page-first').on('click',()=>{
+    fillContainer(--pageNo);
+    pageManager(--pageNo);
+  });
+  $('.page-third').on('click',()=>{
+    fillContainer(pageNo++);
+    pageManager(pageNo++);
+  });
+  $('.next').on('click',()=>{
+    fillContainer(pageNo++);
+    pageManager(pageNo++);
+  });
+  $('.last-page').on('click',()=>{
+    fillContainer(lastPage);
+    pageManager(lastPage);
+  });
+
   const fillContainer = (pageNo)=>{
     paintings.map((item)=>{
       if (item.position>15*(pageNo-1)&&item.position<=15*pageNo){
@@ -91,7 +118,7 @@ $(document).ready(
         }
       }
     }
-
+    // helper function for visibility - hides or reveals the pages buttons -
     const assignerVisibility=(prev, dots1, one, two, three,dots2,next)=>{
       if (prev){
         $('.previous').removeClass('.no-display');
@@ -130,8 +157,9 @@ $(document).ready(
       }
     }
 
-
+    visibility(pageNo);
+    centralPages(pageNo);
   }
 
-  const lastPage=()=>Math.ceil(paintings.length/15);
+
 );
