43554201279ff2fe732553fef325dee9c4c4372a
Bogdan5 <bogdan5x@gmail.com>
2018-03-30 23:40:58 -0400

    'Comment'

--- javascripts/JavaScript.js
+++ javascripts/JavaScript.js
@@ -1,18 +1,19 @@
-$(document).ready(()=>{
-  let paintings=[];
+$(document).ready(() => {
+  let paintings = [];
   let pageNo = 1;
-  let currPage=1;
+  let currPage = 1;
 
   fetch('https://raw.githubusercontent.com/Bogdan5/art-gallery/master/data.json')
-    .then((response)=>{
-      if (response.status!==200){
-        console.log("Data unavailable "+response.status);
+    .then((response)=> {
+      if (response.status !== 200) {
+        console.log('Data unavailable ' + response.status);
         return;
       }
+
       response.json().then((data)=> {
-        let i=0;
-        data.map((item,index)=>{
-          let obj = Object.assign({},item);
+        let i = 0;
+        data.map((item, index) => {
+          let obj = Object.assign({}, item);
           obj.position = ++i;
           paintings.push(obj);
         });
@@ -24,137 +25,168 @@ $(document).ready(()=>{
       console.log('Fetch Error :-S', err);
     });
 
-  const lastPage=()=>Math.ceil(paintings.length/15);
-  const eventAdder = (obj)=>{
-    return ()=>{
-      $('.viewer').empty().append('<img src=\"'+obj.url+'\"/>')
-      .append('<div>Author: '+obj.author+'</div>')
-      .append('<div>Title: '+obj.title+'</div>')
-      .append('<div>Type: '+obj.type+'</div>')
-      .append('<div>Year: '+obj.year+'</div>')
-      .append('<div>Reserve: '+obj.reserve+'</div>')
-      .append('<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalDiv">More information</button>');
-    }
-  }
-  const fillContainer = (pageNo)=>{
+  const lastPage = () => Math.ceil(paintings.length / 15);
+  const eventAdder = (obj) => {
+    return () => {
+      $('.viewer').empty().append('<img src=\"' + obj.url + '\"/>')
+      .append('<div>Author: ' + obj.author + '</div>')
+      .append('<div>Title: ' + obj.title + '</div>')
+      .append('<div>Type: ' + obj.type + '</div>')
+      .append('<div>Year: ' + obj.year + '</div>')
+      .append('<div>Reserve: ' + obj.reserve + '</div>')
+      .append('<button type="button" class="btn btn-primary" data-toggle="modal"  data-target="#modalDiv">More information</button>');
+    };
+  };
+
+  const fillContainer = (pageNo) => {
     pageManager(pageNo);
-    $("#thumbs-container").empty();
-    paintings.map((item)=>{
-      if (item.position>15*(pageNo-1)&&item.position<=15*pageNo){
-        $("#thumbs-container").append('<div id=\"card'+item.position+'\"></div>');
-        $("#card"+item.position).addClass("col-lg-4 col-md-6 col-sm-12 card")
-          .append('<img src=\"'+item.url+'\"/>').append('<div class=\"author-card\">'+item.author+item.position+'</div>')
-          .append('<div class=\"title-card\">'+item.title+'</div>');
-          $('#card'+item.id).on('click',eventAdder(item));
+    $('#thumbs-container').empty();
+    paintings.map((item) => {
+      if (item.position > 15 * (pageNo - 1) && item.position <= 15 * pageNo) {
+        $('#thumbs-container').append('<div id=\"card' + item.position + '\"></div>');
+        $('#card' + item.position).addClass('col-lg-4 col-md-6 col-sm-12 card')
+          .append('<img src=\"' + item.url + '\"/>').append('<div class=\"author-card\">'
+          + item.author + item.position + '</div>')
+          .append('<div class=\"title-card\">' + item.title + '</div>');
+        $('#card' + item.id).on('click', eventAdder(item));
       }
 
     });
-  }
+  };
 
-  const eventPages = (pageNo)=>{
-    $('#first-page').on('click',()=>{
+  const eventPages = (pageNo) => {
+    $('#first-page').on('click', () => {
       fillContainer(1);
     });
-    $('#previous').on('click',()=>{
+    $('#previous').on('click', () => {
       fillContainer(--pageNo);
     });
-    $('#page-first').on('click',()=>{
-      if (pageNo!==1){
+    $('#page-first').on('click', () => {
+      if (pageNo !== 1) {
         fillContainer(--pageNo);
       }
     });
-    $('#page-second').on('click',()=>{
-      if (pageNo===1){
+    $('#page-second').on('click', () => {
+      if (pageNo === 1) {
         fillContainer(++pageNo);
-      } else if (pageNo===lastPage()){
+      } else if (pageNo === lastPage()) {
         fillContainer(--pageNo);
       }
     });
-    $('#page-third').on('click',()=>{
-      if (pageNo!==lastPage()){
-        if (pageNo===1){
-          pageNo+=2;
+    $('#page-third').on('click', () => {
+      if (pageNo !== lastPage()) {
+        if (pageNo === 1) {
+          pageNo += 2;
         } else {
           pageNo++;
         }
+
         fillContainer(pageNo);
       }
     });
-    $('#next').on('click',()=>{
+    $('#next').on('click', () => {
       fillContainer(pageNo++);
     });
-    $('#last-page').on('click',()=>{
+    $('#last-page').on('click', () => {
       fillContainer(lastPage());
     });
-  }
+  };
 
-  const pageManager=(pageNo)=>{
+  const pageManager = (pageNo) => {
     const dots = '<div class="dots">...<div>';
-    const contain = $('#pages-container');
+
+    // const contain = $('#pages-container');
     const last = lastPage();
 
     //gives number values to each page number div
-    const centralPages = (pageNo)=>{
-      if (last<4 || pageNo===1){
-        assignerNumber(1,2,3);
-      } else if (pageNo===last){
-        assignerNumber(pageNo-2,pageNo-1,pageNo);
+    const centralPages = (pageNo) => {
+      if (last < 4 || pageNo === 1) {
+        assignerNumber(1, 2, 3);
+      } else if (pageNo === last) {
+        assignerNumber(pageNo - 2, pageNo - 1, pageNo);
       } else {
-        assignerNumber(pageNo-1, pageNo, pageNo+1);
+        assignerNumber(pageNo - 1, pageNo, pageNo + 1);
       }
-    }
+    };
+
     //helper function for centralPages
-    const assignerNumber = (...arr)=>{
+    const assignerNumber = (...arr) => {
       $('#page-first').text(arr[0]);
       $('#page-second').text(arr[1]);
       $('#page-third').text(arr[2]);
-    }
+    };
 
-    const visibility = (pageNo)=>{
-      console.log(pageNo);
-      if (last===1){
+    const visibility = (pageNo) => {
+      const positionVisibility = (...arg) => {
+        const isVisible = arg.shift();
+        arg.map((el) => {
+          if (isVisible) {
+            $('#pages-container>div').eq(el).removeClass('no-display');
+          } else {
+            $('#pages-container>div').eq(el).addClass('no-display');
+          }
+        });
+      };
+
+      // if (pageNo === 1 || last === )
+
+      if (pageNo === 1 || last === 1) {
+        positionVisibility(false, 0, 1, 2);
+      }
+
+      if (pageNo === last || last === 1) {
+        positionVisibility(false, 6, 7, 8);
+      }
+
+      if (last === 2) {
+        positionVisibility(false, 2, 5, 6);
+      }
+
+      // console.log(pageNo);
+      if (last === 1) {
         assignerVisibility(false, false, false, true, false, false, false, false, false);
-      } else if (last===2){
-        if (pageNo===1){
-          assignerVisibility(false, false, false, true, true , false, true, true);
+      } else if (last === 2) {
+        if (pageNo === 1) {
+          assignerVisibility(false, false, false, true, true, false, true, true);
         } else {
-          assignerVisibility(true, true, false, true, true , false, false, false, false);
+          assignerVisibility(true, true, false, true, true, false, false, false, false);
         }
       } else if (last === 3) {
-        if (pageNo===1){
+        if (pageNo === 1) {
           assignerVisibility(false, false, false, true, true, true, true, true, true);
-        } else if (pageNo === last){
-          assignerVisibility(true,true,false,true,true,true,false,false,false);
+        } else if (pageNo === last) {
+          assignerVisibility(true, true, false, true, true, true, false, false, false);
         } else {
-          assignerVisibility(true,true,false,true,true,true,false,true,true);
+          assignerVisibility(true, true, false, true, true, true, false, true, true);
         }
       } else {
-        if (pageNo===1){
-          assignerVisibility(false,false,false, true, true, true, true, true, true);
-        } else if (pageNo===last){
-          assignerVisibility(true,true,true,true,true,true,false,false,false);
-        } else if (pageNo===2){
-          console.log(2,4);
-          assignerVisibility(true,true,false,true,true,true,true,true,true);
-        } else if (pageNo===last-1){
-          assignerVisibility(true,true,true,true,true,true,false,true,true);
+        if (pageNo === 1) {
+          assignerVisibility(false, false, false, true, true, true, true, true, true);
+        } else if (pageNo === last) {
+          assignerVisibility(true, true, true, true, true, true, false, false, false);
+        } else if (pageNo === 2) {
+          console.log(2, 4);
+          assignerVisibility(true, true, false, true, true, true, true, true, true);
+        } else if (pageNo === last - 1) {
+          assignerVisibility(true, true, true, true, true, true, false, true, true);
         } else {
-          assignerVisibility(true,true,true,true,true,true,true,true,true);
+          assignerVisibility(true, true, true, true, true, true, true, true, true);
         }
       }
-    }
+    };
+
     // helper function for visibility - hides or reveals the pages buttons -
-    const assignerVisibility=(...arg)=>{
-      $('#pages-container>div').each((index,el)=>{
-        if (arg[index]){
+    const assignerVisibility = (...arg) => {
+      $('#pages-container>div').each((index, el) => {
+        if (arg[index]) {
           $(el).removeClass('no-display');
         } else {
           $(el).addClass('no-display');
         }
       });
-    }
+    };
 
     centralPages(pageNo);
     visibility(pageNo);
-  }
+  };
 });
