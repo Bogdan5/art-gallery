984d58e114e201f333d0b6bf3c466dae97519ed9
Bogdan5 <bogdan5x@gmail.com>
2018-04-08 17:26:24 -0400

    'Comment'

--- javascripts/JavaScript.js
+++ javascripts/JavaScript.js
@@ -1,8 +1,4 @@
 $(document).ready(() => {
-  let paintings = [];
-  let pageNo = 1;
-  let currPage = 1;
-
   fetch('https://raw.githubusercontent.com/Bogdan5/art-gallery/master/data.json')
     .then((response)=> {
       let paintings = [];
@@ -18,30 +14,35 @@ $(document).ready(() => {
           obj.position = ++i;
           paintings.push(obj);
         });
-        fillContainer(paintings, 1);
-        eventPages(pageNo);
-        eventSearch();
+        displayManager(paintings, 1);
+        searchManager();
       });
     })
     .catch((err)=> {
       console.log('Fetch Error :-S', err);
     });
+});
 
+//displays the cards container and pagenumbers for a specific data set and pageNo
+const displayManager = (paintings, pageNo) => {
   const lastPage = (paintings) => Math.ceil(paintings.length / 15);
-  const eventAdder = (obj) => {
-    return () => {
-      $('.viewer').empty().append('<img src=\"' + obj.url + '\"/>')
-      .append('<div>Author: ' + obj.author + '</div>')
-      .append('<div>Title: ' + obj.title + '</div>')
-      .append('<div>Type: ' + obj.type + '</div>')
-      .append('<div>Year: ' + obj.year + '</div>')
-      .append('<div>Reserve: ' + obj.reserve + '</div>')
-      .append('<button type="button" class="btn btn-primary" data-toggle="modal"  data-target="#modalDiv">More information</button>');
-    };
-  };
 
   const fillContainer = (paintings, pageNo) => {
-    pageManager(pageNo);
+
+    //helper function that returns a function for adding click events on painting cards
+    const eventAdder = (obj) => {
+      return () => {
+        $('.viewer').empty().append('<img src=\"' + obj.url + '\"/>')
+        .append('<div>Author: ' + obj.author + '</div>')
+        .append('<div>Title: ' + obj.title + '</div>')
+        .append('<div>Type: ' + obj.type + '</div>')
+        .append('<div>Year: ' + obj.year + '</div>')
+        .append('<div>Reserve: ' + obj.reserve + '</div>')
+        .append('<button type="button" class="btn btn-primary" data-toggle="modal"  data-target="#modalDiv">More information</button>');
+      };
+    };
+
+    //clears the container and fills it with cards for each painting
     $('#thumbs-container').empty();
     paintings.map((item) => {
       if (item.position > 15 * (pageNo - 1) && item.position <= 15 * pageNo) {
@@ -52,52 +53,12 @@ $(document).ready(() => {
           .append('<div class=\"title-card\">' + item.title + '</div>');
         $('#card' + item.id).on('click', eventAdder(item));
       }
-
     });
+    pageManager(pageNo);
   };
 
-  const eventPages = (paintings, pageNo) => {
-    $('#first-page').on('click', () => {
-      fillContainer(paintings, 1);
-    });
-    $('#previous').on('click', () => {
-      fillContainer(paintings, --pageNo);
-    });
-    $('#page-first').on('click', () => {
-      if (pageNo !== 1) {
-        fillContainer(paintings, --pageNo);
-      }
-    });
-    $('#page-second').on('click', () => {
-      if (pageNo === 1) {
-        fillContainer(paintings, ++pageNo);
-      } else if (pageNo === lastPage(paintings)) {
-        fillContainer(paintings, --pageNo);
-      }
-    });
-    $('#page-third').on('click', () => {
-      if (pageNo !== lastPage()) {
-        if (pageNo === 1) {
-          pageNo += 2;
-        } else {
-          pageNo++;
-        }
-
-        fillContainer(paintings, pageNo);
-      }
-    });
-    $('#next').on('click', () => {
-      fillContainer(paintings, pageNo++);
-    });
-    $('#last-page').on('click', () => {
-      fillContainer(paintings, lastPage());
-    });
-  };
-
+  //manages page buttons establishes visibility and binds events
   const pageManager = (pageNo) => {
-    const dots = '<div class="dots">...<div>';
-
-    // const contain = $('#pages-container');
     const last = lastPage();
 
     //gives number values to each page number div
@@ -111,7 +72,7 @@ $(document).ready(() => {
       }
     };
 
-    //helper function for centralPages
+    //helper function for centralPages - add page numbers to buttons
     const assignerNumber = (...arr) => {
       $('#page-first').text(arr[0]);
       $('#page-second').text(arr[1]);
@@ -164,19 +125,72 @@ $(document).ready(() => {
       $('div[id ^= "page-"]').not('#page-' + select).removeClass('pages-pressed');
     };
 
+    //adds events listeners for all buttons of the page numbers
+    const eventPages = (paintings, pageNo) => {
+      $('#first-page').on('click', () => {
+        fillContainer(paintings, 1);
+      });
+      $('#previous').on('click', () => {
+        fillContainer(paintings, --pageNo);
+      });
+      $('#page-first').on('click', () => {
+        if (pageNo !== 1) {
+          fillContainer(paintings, --pageNo);
+        }
+      });
+      $('#page-second').on('click', () => {
+        if (pageNo === 1) {
+          fillContainer(paintings, ++pageNo);
+        } else if (pageNo === lastPage(paintings)) {
+          fillContainer(paintings, --pageNo);
+        }
+      });
+      $('#page-third').on('click', () => {
+        if (pageNo !== lastPage()) {
+          if (pageNo === 1) {
+            pageNo += 2;
+          } else {
+            pageNo++;
+          }
+
+          fillContainer(paintings, pageNo);
+        }
+      });
+      $('#next').on('click', () => {
+        fillContainer(paintings, pageNo++);
+      });
+      $('#last-page').on('click', () => {
+        fillContainer(paintings, lastPage());
+      });
+    };
+
     centralPages(pageNo);
     visibility(pageNo);
     pageColours(pageNo);
   };
 
-  const eventSearch = (paintings) => $('#search').on('click', (paintings) => {
-    let word = $('#searchTerm').val();
+  fillContainer(paintings, pageNo);
+};
+
+const searchManager = () => {
+
+  //filtered is the array with all the cards that fit the search terms and type
+  let filtered = paintings.slice();
+  $('#searchTerm').on('input', (event) => {
+    let word =  $(event.currentTarget).val();
     let opt = $('#typeSearch').val();
-    switch (opt) {
-      case 1:
-        paintings.filter();
+    if (word) {
+      if (opt === 1) {
+        filtered.filter((el) => {
+          Object.entries(el).reduce((acc, elem) => {
+            acc = acc || word.test(elem);
+          }, false);
+        });
+      } else {
+        filtered.filter(el => word.test(el[opt]));
+      }
     }
 
+    displayManager(filtered, 1);
   });
-
-});
+};
